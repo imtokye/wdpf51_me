7b0fc41cc4ffeed43ba7b37f657426fda74f6787
unknown <you@example.com>
2022-09-11 19:01:17 +0600

    toky

---
+++ Projects/project_excercise/user-registration-verification/db.php
@@ -0,0 +1,8 @@
+<?php
+    $servername='localhost';
+    $username='root';
+    $password='';
+    $dbname = " wdpf51_excercise_ch1";
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
+      
+?>
